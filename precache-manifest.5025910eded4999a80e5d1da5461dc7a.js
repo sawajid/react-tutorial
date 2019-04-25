7e2ca2a9554b3b48fcd96905353c447ad15f808e
Wajid <syedwaj@gmail.com>
2019-04-25 21:31:28 +0530

    Updates

---
+++ precache-manifest.5025910eded4999a80e5d1da5461dc7a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a06b51f3fd3b41ae08a2bd23d88e27d6",
+    "url": "/react-tutorial/index.html"
+  },
+  {
+    "revision": "0d07da290459660adf2e",
+    "url": "/react-tutorial/static/css/main.545ae54c.chunk.css"
+  },
+  {
+    "revision": "3a9e33913cab0720716d",
+    "url": "/react-tutorial/static/js/2.86a2265c.chunk.js"
+  },
+  {
+    "revision": "0d07da290459660adf2e",
+    "url": "/react-tutorial/static/js/main.77162bc7.chunk.js"
+  },
+  {
+    "revision": "2a4d04de927634e03e5b",
+    "url": "/react-tutorial/static/js/runtime~main.782a6e4d.js"
+  }
+]);
